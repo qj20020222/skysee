f9ed127921423ee690c7da3d4fa44e068ac10c4b
qj20020222 <jian.qi@gwmail.gwu.edu>
2025-01-20 13:32:48 +0800

    12

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,6 +6,6 @@ export default {
   dialect: 'postgresql',
   schema: ".libs/db/schema.ts",
   dbCredentials: {
-    connectionString: "postgres://qj20020222:qjissb_2002@mydb-instance.rds.amazonaws.com:5432/mydatabase"
+    url: process.env.POSTGRES_URL!,
   },
 } satisfies Config;
